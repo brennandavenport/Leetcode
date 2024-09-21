2e08cbddf846c14508a7c8e93bdc16ceafa3fbc0
Brennan Davenport <brennandavenport9@gmail.com>
2024-09-21 18:22:59 -0400

    daily

--- SlidingWindow/problem_121.py
+++ SlidingWindow/problem_121.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy = 0
@@ -14,3 +17,6 @@ class Solution:
 
         return maxProfit 
 
+s = Solution()
+
+print(s.maxProfit([2,4,5,62,124,5265,352343,442,24,5,9]))
